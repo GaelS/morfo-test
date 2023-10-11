5d49e7f236c648c9c5f5996f3199266baf4b5e8b
Gael S <servauga@gmail.com>
2023-10-11 20:54:03 +0200

    trial to remove nextjs caching on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { cache } from "react";
 import { AddSpeciesLink } from "@/components/AddSpeciesLink";
 import { NoSpeciesFoundFallback } from "@/components/NoSpeciesFoundFallback";
 import { SpeciesList } from "@/components/SpeciesList";
 import { speciesUseCase } from "./api/useCases/species";
 import { PageTitle } from "@/design-system/PageTitle";
+
+/**
+ * All in to prevent nextjs caching in vain...
+ */
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const getData = async function () {
   const data = await speciesUseCase.getAllSpecies();
